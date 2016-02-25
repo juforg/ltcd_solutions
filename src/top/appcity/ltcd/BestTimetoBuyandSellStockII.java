8df8b8c1f30b5ebfcbd5025baf0032fd24a24c13
juforg <juforg@sina.com>
2016-02-25 22:14:57 +0800

    first commit

---
+++ src/top/appcity/ltcd/BestTimetoBuyandSellStockII.java
@@ -0,0 +1,19 @@
+package top.appcity.ltcd;
+
+public class BestTimetoBuyandSellStockII {
+	
+	public int maxProfit(int[] prices) {
+        for(int i = 1; i<prices.length;i++){
+             
+        }
+        return 0;
+    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
